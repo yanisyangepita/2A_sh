b72e0b439264d96cc87fa159dd6dec206867df75
Cyril BARBEL <cyril.barbel@epita.fr>
2022-05-03 17:45:48 +0200

    echo parser ready WHOUHOUUUUUUUUU LETS GOOOOOOOO

--- src/parser.c
+++ src/parser.c
@@ -213,10 +213,108 @@ s_ast *found_type(s_token_list *tkl, size_t *current, size_t end,
     return ast;
 }
 
+void horrible_func(char ***tab_files, size_t *len_files,
+        char ***tab_opt_files, size_t *len_opt_files,
+        char **tab_param, s_token tok)
+{
+    char **files = *tab_files;
+    char **opt_files = *tab_opt_files;
+    char *param = *tab_param;
+    size_t i = 0;
+
+    char *str = malloc(sizeof(char));
+    str[0] = 0;
+    size_t i_str = 0;
+    size_t file = 0;
+
+    while (tok.str[i] != 0)
+    {
+        if (tok.str[i] == ' ' && file == 1)
+        {
+            //files append
+            files = realloc(files, sizeof(char *) * (*len_files + 1));
+            files[*len_files] = str;
+            str = malloc(sizeof(char));
+            str[0] = 0;
+            (*len_files)++;
+            i_str = 0;
+
+            file = 0;
+        }
+        else if (tok.str[i] == '>')
+        {
+            //indent append str
+            param = str_concat(param, str);
+            str = malloc(sizeof(char));
+            str[0] = 0;
+            i_str = 0;
+
+            if (tok.str[i + 1] == '>')
+            {
+                opt_files = realloc(opt_files, sizeof(char *) * (*len_opt_files + 1));
+                opt_files[*len_opt_files] = ">>";
+                i++;
+            }
+            else
+            {
+                //opt_files append ">"
+                opt_files = realloc(opt_files, sizeof(char *) * (*len_opt_files + 1));
+                opt_files[*len_opt_files] = ">";
+            }
+            //next is file until ' '
+            (*len_opt_files)++;
+            if (tok.str[i + 1] == ' ')
+                i++;
+            file = 1;
+        }
+        else
+        {
+            //str append char
+            str = realloc(str, sizeof(char) * (i_str + 2));
+            str[i_str] = tok.str[i];
+            i_str++;
+            str[i_str] = 0;
+        }
+        i++;
+    }
+    if (i_str != 0)
+    {
+        if (file == 1)
+        {
+            files = realloc(files, sizeof(char *) * (*len_files + 1));
+            files[*len_files] = str;
+            str = malloc(sizeof(char));
+            str[0] = 0;
+            (*len_files)++;
+            i_str = 0;
+        }
+        else
+        {
+            param = str_concat(param, str);
+            str = malloc(sizeof(char));
+            str[0] = 0;
+            i_str = 0;
+        }
+    }
+    *tab_param = param;
+    *tab_opt_files = opt_files;
+    *tab_files = files;
+    free(str);
+}
+
 void parse_echo(s_ast *ast, s_token_list *tkl, size_t current, size_t end)
 {
     ast->left = found_type(tkl, &current, end, OPTION);
     ast->right = found_type(tkl, &current, end, IDENTIFIER);
+
+    char **files = malloc(sizeof(char *));
+    size_t len_files = 0;
+    char **opt_files = malloc(sizeof(char *));
+    size_t len_opt_files = 0;
+    char *param = malloc(sizeof(char));
+    param[0] = 0;
+    horrible_func(&files, &len_files, &opt_files,
+            &len_opt_files, &param, ast->right->token);
 
     size_t len_opt = 0;
     char **opt = NULL;
@@ -226,6 +324,13 @@ void parse_echo(s_ast *ast, s_token_list *tkl, size_t current, size_t end)
 #ifdef DEBUG
     for (size_t i = 0; i < len_opt; i++)
         printf("opt : %s\n", opt[i]);
+
+    for (size_t i = 0; i < len_files; i++)
+        printf("files : %s\n", files[i]);
+
+    for (size_t i = 0; i < len_opt_files; i++)
+        printf("opt_files : %s\n", opt_files[i]);
+    printf("param : %s\n", param);
 #endif
     if (opt != NULL)
     {
@@ -233,6 +338,15 @@ void parse_echo(s_ast *ast, s_token_list *tkl, size_t current, size_t end)
             free(opt[i]);
         free(opt);
     }
+
+    if (files != NULL)
+    {
+        for (size_t i = 0; i < len_files; i++)
+            free(files[i]);
+        free(files);
+    }
+    free(param);
+    free(opt_files);
 }
 
 void parse_cat(s_ast *ast, s_token_list *tkl, size_t current, size_t end)
